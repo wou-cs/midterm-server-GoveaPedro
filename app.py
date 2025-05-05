84dc0659a1cd86c61a5fa0d0f63f66e4330b7e3c
YNihility <pgovea21@mail.wou.edu>
2025-05-04 17:08:36 -0700

    Implemented API, returning dec and hex

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/api/calcs/<value>", methods=["GET"])
+def method_name(value):
+    try:
+        num = int(value)
+        if num <= 0:
+            raise ValueError
+        return jsonify({
+            "dec": num - 1,
+            "hex": hex(num)
+        })
+    except ValueError:
+        return "Invalid input", 400
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
